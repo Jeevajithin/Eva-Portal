cd50c7221243c1b3850ee042e03da6bacf8efd31
JeevaJithin <jeeva.sics@gmail.com>
2024-01-05 17:58:38 +0530

    modified view_more_users page

--- resources/views/view_more_users.blade.php
+++ resources/views/view_more_users.blade.php
@@ -49,6 +49,42 @@
                         </div>
                     </a>
                 </div>
+                <div class="col-lg-8">
+                <div class="card">
+                  <div class="table-responsive">
+                    <table class="table table-vcenter card-table">
+                      <thead>
+                        <tr>
+                          <th>Admission No</th>
+                          <th>Name</th>
+                          <th>Course</th>
+                          <th>Total Fee</th>
+                          <th>Fee Paid</th> 
+                          <th class="w-1"></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>SC5465</td>
+                          <td class="text-muted">Archana</td>
+                          <td class="text-muted">Software Testing</td>
+                          <td><b>600000</b></td>
+                          <td>10000</td>
+                          <td><a href="#">view</a></td>
+                        </tr>
+                        <tr>
+                          <td>SC5465</td>
+                          <td class="text-muted">Sarath Chandran</td>
+                          <td class="text-muted">Python</td>
+                          <td><b>600000</b></td>
+                          <td>10000</td>
+                          <td><a href="#">view</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
 
                 <div class="col-lg-4">
                 <div class="card">
@@ -62,8 +98,28 @@
                         </tr>
                       </thead>
                       <tbody>
+                      <tr>
+                          <td><div class="progressbg-text">Employee ID</div></td>
+                          <td class="w-1 fw-bold text-end">5454</td>
+                        </tr>
                         <tr>
-                          <td><div class="progressbg-text">Contact Person</div></td>
+                          <td><div class="progressbg-text">Name</div></td>
+                          <td class="w-1 fw-bold text-end">Irfana</td>
+                        </tr>
+                        <tr>
+                          <td><div class="progressbg-text">Email ID</div></td>
+                          <td class="w-1 fw-bold text-end">irfana@gmail.com</td>
+                        </tr>
+                        <tr>
+                          <td><div class="progressbg-text">Contact Number</div></td>
+                          <td class="w-1 fw-bold text-end">957896958</td>
+                        </tr>
+                        <tr>
+                          <td><div class="progressbg-text">Designation</div></td>
+                          <td class="w-1 fw-bold text-end">Sales Associates</td>
+                        </tr>
+                        <tr>
+                          <td><div class="progressbg-text">Reporting Person</div></td>
                           <td class="w-1 fw-bold text-end">Monisha</td>
                         </tr> 
                         <tr>
@@ -73,19 +129,7 @@
                         <tr>
                           <td><div class="progressbg-text">Team Lead</div></td>
                           <td class="w-1 fw-bold text-end">Nida</td>
-                        </tr> 
-                        <tr>
-                          <td><div class="progressbg-text">Contact Number</div></td>
-                          <td class="w-1 fw-bold text-end">9576897456</td>
-                        </tr> 
-                        <tr>
-                          <td><div class="progressbg-text">Email Id</div></td>
-                          <td class="w-1 fw-bold text-end">Irfana@gmail.com</td>
                         </tr>   
-                        <tr>
-                          <td><div class="progressbg-text">Demo</div></td>
-                          <td class="w-1 fw-bold text-end">Dummy Text</td>
-                        </tr>     
                       </tbody>
                     </table>
                   </div>
